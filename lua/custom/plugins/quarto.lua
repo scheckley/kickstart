6cbd54ada1aeef55b5a9040f885373ffdb2f1036
Stephen Checkley <scheckley@users.noreply.github.com>
2023-11-03 23:01:48 +0000

    added quarto config

--- lua/custom/plugins/quarto.lua
+++ lua/custom/plugins/quarto.lua
@@ -2,4 +2,26 @@
 
 return {
   "quarto-dev/quarto-nvim",
+  config = {
+    debug = false,
+  closePreviewOnExit = true,
+  lspFeatures = {
+    enabled = true,
+    languages = { 'r', 'python', 'julia', 'bash' },
+    chunks = 'curly', -- 'curly' or 'all'
+    diagnostics = {
+      enabled = true,
+      triggers = { "BufWritePost" }
+    },
+    completion = {
+      enabled = true,
+    },
+  },
+  keymap = {
+    hover = 'K',
+    definition = 'gd',
+    rename = '<leader>lR',
+    references = 'gr',
+  }
+}
 }
